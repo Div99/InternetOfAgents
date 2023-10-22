e9199942a71c7589287f55526f245efee82bc094
Sasha <sashashengyt@gmail.com>
2023-10-21 17:23:18 -0700

    task

--- mapr.py
+++ mapr.py
@@ -4,7 +4,7 @@ from datetime import timedelta
 import openai
 import guidance
 import requests
-from prefect.task_runners import SequentialTaskRunner
+from prefect.task_runners import ConcurrentTaskRunner
 import os
 from dotenv import load_dotenv
 load_dotenv()
@@ -15,11 +15,11 @@ gpt4 = guidance.llms.OpenAI("gpt-4")
 
 list_prompt = """
 {{#system~}}
-You are an expert task manager that manages agents that each does one task. You decide how many parallel agent is needed to do the meta-task, only list the websites required for each:
+You are an expert task manager that manages parallel agents that each does one task, only list the websites that made sense for the task:
 {{~/system}}
 
 {{#user~}}
-Find Top 10 Frontend Engineers
+Find Top 10 Frontend Engineers in the Austen area
 Answer:
 https://www.linkedin.com
 https://www.indeed.com
@@ -28,39 +28,42 @@ https://www.glassdoor.com
 {{query}}
 Answer:
 {{~/user}}
-
 {{#assistant~}}
 {{gen 'answers' temperature=0 max_tokens=300}}
 {{~/assistant}}
 """
 
-reduce_prompt = """
-{{#system~}}
-You are an expert task manager that aggregates results returned by each agent.:
-{{~/system}}
+# reduce_prompt = """
+# {{#system~}}
+# You are an expert task manager that aggregates results returned by each agent.:
+# {{~/system}}
 
-{{#user~}}
-Aggregate the following: 
-Answer:
-https://www.linkedin.com
-https://www.indeed.com
-https://www.glassdoor.com
+# {{#user~}}
+# Aggregate the following: 
+# Answer:
+# https://www.linkedin.com
+# https://www.indeed.com
+# https://www.glassdoor.com
 
-{{query}}
-Answer:
-{{~/user}}
+# {{query}}
+# Answer:
+# {{~/user}}
 
-{{#assistant~}}
-{{gen 'aggregated' temperature=0 max_tokens=300}}
-{{~/assistant}}
-"""
+# {{#assistant~}}
+# {{gen 'aggregated' temperature=0 max_tokens=300}}
+# {{~/assistant}}
+# """
 
 @task
 def generate_a_list(input: str = None) -> list:
     # Generate a list of queries or platforms to search for frontend engineers in the bay area.
     experts = guidance(list_prompt, llm=gpt4)
     executed_program = experts(query=input) # Example platforms
-    return executed_program["answers"].split("\n")
+    print("executed_program")
+    print(executed_program)
+    urls = executed_program["answers"].split("\n")
+    print(f"Returned URLS are: {urls}")
+    return urls
 
 @task
 def reduce(platforms: list) -> list:
@@ -88,6 +91,7 @@ def notify_user(final_result: list) -> None:
 def _login():
     multion.login()
     _ = multion.set_remote(False)
+    print("Logged in...")
 
 @task(retries=3, retry_delay_seconds=10)
 def execute_single_agent_task(input: str = None, url: str = "https://www.google.com", tabId: str = None):
@@ -95,20 +99,26 @@ def execute_single_agent_task(input: str = None, url: str = "https://www.google.
     new_input = input + ". Do not ask for user input." 
     session = multion.new_session(data={"input": new_input, "url": url})
     tabId = session['session_id']
+    print(f"Session ID: {tabId}")
 
     updated_session = multion.update_session(tabId=tabId, data={"input": new_input, "url": url})
+    tabId = updated_session['session_id']
     print("updated_session")
     print(list(updated_session.keys()))
     should_continue = updated_session["status"] == "CONTINUE"
-    while should_continue:
-        updated_session = multion.update_session(tabId=tabId, data={"input": new_input, "url": url})
-        should_continue = updated_session["status"] == "CONTINUE"
-        print("updated_session")
-        print(list(updated_session.keys()))
+    try:
+      while should_continue:
+          updated_session = multion.update_session(tabId=tabId, data={"input": new_input, "url": updated_session["url"]})
+          should_continue = updated_session["status"] == "CONTINUE"
+          print("updated_session")
+          print(list(updated_session.keys()))
+          tabId = updated_session['session_id']
+    except Exception as e:
+      print(f"ERROR: {e}")
         
     closed_session = multion.close_session(tabId=tabId)
     print("closed session")
-    print("URL: ", closed_session['url'])
+    print(list(closed_session.keys()))
     print("Session ID: ", closed_session['session_id'])
     print("Message: ", closed_session['message'])
     print("Status: ", closed_session['status'])
@@ -116,14 +126,14 @@ def execute_single_agent_task(input: str = None, url: str = "https://www.google.
 
 
 @flow(name="My Flow",
-      task_runner=SequentialTaskRunner())
+      task_runner=ConcurrentTaskRunner())
 def main(task="Find Top 10 Frontend Engineers"):
-    platforms = generate_a_list()
+    platforms = generate_a_list(task)
     reduced_platforms = reduce(platforms)
     sessions = execute_single_agent_task.map(url=reduced_platforms, input=task)
     final_result = final_reduce(sessions)
     notification = notify_user(final_result)
 
 
-main("Find top 10 frontend engineer individuals in the bay area.")
-
+# main("Post on social media saying 'hi, hope you are having a great day!'")
+main("Find Top 10 Frontend Engineers")
