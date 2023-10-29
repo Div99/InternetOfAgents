aa3e63fea2cb88ac7cbba3fea9475a46b958ecf7
Div Garg <divyansh199@gmail.com>
2023-10-29 13:46:24 -0700

    add browse.py

--- mapr2.py
+++ mapr2.py
@@ -1,7 +1,9 @@
-from prefect import task
+from prefect import task, flow
 import requests
 import random
 import string
+import multion
+from prefect.task_runners import ConcurrentTaskRunner
 
 
 # Function to generate a random email
@@ -12,10 +14,56 @@ def generate_random_email() -> str:
     return f"{random_name}@{random_domain}"
 
 
+def _login():
+    multion.login()
+    _ = multion.set_remote(False)
+    print("Logged in...")
+
+
+@task(retries=3, retry_delay_seconds=10)
+def execute_single_agent_task(
+    input: str = None, url: str = "https://www.google.com", tabId: str = None
+):
+    _login()
+    new_input = input + ". Do not ask for user input."
+    session = multion.new_session(data={"input": new_input, "url": url})
+    tabId = session["session_id"]
+    print(f"Session ID: {tabId}")
+
+    updated_session = multion.update_session(
+        tabId=tabId, data={"input": new_input, "url": url}
+    )
+    tabId = updated_session["session_id"]
+    print("updated_session")
+    print(list(updated_session.keys()))
+    should_continue = updated_session["status"] == "CONTINUE"
+    try:
+        while should_continue:
+            updated_session = multion.update_session(
+                tabId=tabId, data={"input": new_input, "url": updated_session["url"]}
+            )
+            should_continue = updated_session["status"] == "CONTINUE"
+            print("updated_session")
+            print(list(updated_session.keys()))
+            tabId = updated_session["session_id"]
+    except Exception as e:
+        print(f"ERROR: {e}")
+
+    closed_session = multion.close_session(tabId=tabId)
+    print("closed session")
+    print(list(closed_session.keys()))
+    print("Session ID: ", closed_session["session_id"])
+    print("Message: ", closed_session["message"])
+    print("Status: ", closed_session["status"])
+    return closed_session
+
+
 @task
-def perform_actions() -> dict:
+def perform_actions(tasks) -> dict:
     # Generate a random email
     email = generate_random_email()
+
+    _login()
 
     # Command to like the post and subscribe
     input_command = f"like the post at https://divgarg.substack.com/p/software-3 and subscribe using email {email}"
@@ -29,6 +77,13 @@ def perform_actions() -> dict:
     return response.json()
 
 
+@task
+def final_reduce(sessions: list) -> list:
+    # Aggregate the results of the Multion sessions to get the top 10 frontend engineers.
+    # For simplicity, we are returning the sessions as-is in this example.
+    return sessions
+
+
 # Function to notify user (can be used to log the result or send a notification)
 @task
 def notify_user(action_results: list) -> None:
@@ -36,15 +91,28 @@ def notify_user(action_results: list) -> None:
         print(f"Notification to User: {result}")
 
 
-from prefect import Flow
+from prefect import Flow, unmapped
 
-with Flow("Like_and_Subscribe") as flow:
-    # Since we're running three tasks in parallel, we use Prefect's mapping to execute the same task with different inputs.
+
+@flow(name="My Flow", task_runner=ConcurrentTaskRunner())
+def main(task="Like_and_Subscribe"):
+    # Generate a list of tasks to be performed
+
+    # Generate a random email
+    email = generate_random_email()
+
+    input_command = f"like the post at https://divgarg.substack.com/p/software-3 and subscribe using email {email}"
+
+    tasks = [input_command] * 3
+    urls = ["https://divgarg.substack.com/p/software-3"] * 3
+
+    # Since we're running multiple tasks in parallel, we use Prefect's mapping to execute the same task with different inputs.
     # In this case, since the input is constant, we use 'unmapped' to prevent Prefect from trying to map over it.
-    action_results = perform_actions.map(unmapped("constant_input"))
-
+    sessions = execute_single_agent_task.map(input=tasks, url=urls)
+    final_result = final_reduce(sessions)
     # Notify the user; this could also be sending an email, logging the result, etc.
-    notification = notify_user(action_results)
+    notification = notify_user(final_result)
+
 
 # Run the flow
-flow.run()
+main()
