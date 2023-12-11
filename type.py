6a609852c4aaf68f638cc5deead05a9e94d1c998
Divyansh Garg <Div99@users.noreply.github.com>
2023-12-10 22:01:45 -0800

    Update type.py

--- type.py
+++ type.py
@@ -23,23 +23,9 @@ class Task(BaseModel):
     cmd: str
     url: str
     status: Status = Status.NOT_ACTIVE
-    # priority: PriorityEnum
-    # assignees: List[str]
-    # subtasks: Optional[List[Subtask]]
-    # dependencies: Optional[List[int]]
-    # owner: ?
-    # worker ?
 
 
 class TaskList(BaseModel):
     """Correctly resolved set of tasks from the given objective"""
 
     tasks: List[Task]
-
-
-class VerifyResult(BaseModel):
-    """Represents verification result of a task. if the task is not verified to be finished successfully, the feedback should be populated with the reason why the task failed and correction the agent can take to fix it."""
-
-    task_id: int
-    verified_done: bool
-    feedback: Optional[str]
